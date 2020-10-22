49b87703eb1724a45cab3100da2052deeecb7a4d
pocari <caffelattenonsugar@gmail.com>
2020-10-22 13:22:04 +0900

    frog追加

---
+++ chap5/frog.rb
@@ -0,0 +1,27 @@
+def read_int
+  gets.chomp.to_i
+end
+
+def main
+  n = read_int
+  h = n.times.map { read_int }
+  inf = h.max + 1
+
+  # p [:n, n, :h, h]
+  dp = Array.new(n, inf)
+  dp[0] = 0
+
+  1.upto(n - 1) do |i|
+    dp[i] = dp[i - 1] + (h[i] - h[i - 1]).abs
+    if i - 2 >= 0
+      i2 = dp[i - 2] + (h[i] - h[i - 2]).abs
+      if i2 < dp[i]
+        dp[i] = i2
+      end
+    end
+  end
+
+  p dp[n-1]
+end
+
+main
