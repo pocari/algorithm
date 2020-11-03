639c9cc583ffd5d9f24b666dabe9237b704a556c
pocari <caffelattenonsugar@gmail.com>
2020-11-03 17:43:02 +0900

    lower_bound, upper_boundを意味を勘違いしていたので修正

--- chap6/q6_2.rb
+++ chap6/q6_2.rb
@@ -1,9 +1,26 @@
-# aryのうち、vより大きい要素のうちもっとも小さい値のindex
-def lower_bound(ary, v, left, right)
+# ソート済みのaryから、vよりお大きい値が初めて出てくるindexを探す
+def upper_bound(ary, v, left, right)
   return nil if left > right
 
   mid = (left + right) / 2
   if ary[mid] > v
+    idx = upper_bound(ary, v, left, mid - 1)
+    if idx
+      idx
+    else
+      mid
+    end
+  else
+    upper_bound(ary, v, mid + 1, right)
+  end
+end
+
+# ソート済みのaryから、v以上の値が初めて出てくるindexを探す
+def lower_bound(ary, v, left, right)
+  return nil if left > right
+
+  mid = (left + right) / 2
+  if ary[mid] >= v
     idx = lower_bound(ary, v, left, mid - 1)
     if idx
       idx
@@ -15,41 +32,22 @@ def lower_bound(ary, v, left, right)
   end
 end
 
-# aryのうち、vより小さい要素のうちもっとも大きい値のindex
-def upper_bound(ary, v, left, right)
-  return nil if left > right
-
-  mid = (left + right) / 2
-  if ary[mid] < v
-    idx = upper_bound(ary, v, mid + 1, right)
-    if idx
-      idx
-    else
-      mid
-    end
-  else
-    upper_bound(ary, v, left, mid - 1)
-  end
-end
-
 def main
   nn = gets.chomp.to_i
   as = gets.chomp.split.map(&:to_i).sort
   bs = gets.chomp.split.map(&:to_i).sort
   cs = gets.chomp.split.map(&:to_i).sort
 
-  ans = 0
-
   # 真ん中のBの行からみて、
-  # a側はbの値よりも小さい値のうち最大のindexまで(upper_bound)
-  # c側はbの値よりも大きい値のうち最小のindexまで(lower_bound)
+  # 1, 2, 3, 4, 5, 6, 7, 8
+  # a側はbの値以上の値が初めてでくるindex(lower_bound)未満までの値が使える
+  # c側はbの値よりも大きい値が初めて出てくるindex(upper_boun)以上の値を使える
   # が対象になるので、そこまでの個数を掛けたものをBの全要素について足し合わせる
+  ans = 0
   bs.each do |b|
-    a_index = upper_bound(as, b, 0, nn - 1)
-    c_index = lower_bound(cs, b, 0, nn - 1)
-    if a_index && c_index
-      ans += (a_index + 1) * (nn - c_index)
-    end
+    a_index = lower_bound(as, b, 0, nn - 1) || nn # B以上ものがなかったら全部使えるのでnn個になるように調整
+    c_index = upper_bound(cs, b, 0, nn - 1) || nn # Bより大きいものがなかったら全部使えないので0になるように調整
+    ans += a_index * (nn - c_index)
   end
 
   puts ans
